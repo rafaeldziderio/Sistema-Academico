91880870b95a0cc6d88cbde4bd3f3f06d84ca80a
rafaeldziderio <rafaeldezideriovilela@gmail.com>
2018-12-08 19:46:21 -0200

    Alterei TiposDocumentos e TiposLogradouros

--- src/sistemaacademico/TiposDocumentos.java
+++ src/sistemaacademico/TiposDocumentos.java
@@ -1,22 +1,13 @@
 package sistemaacademico;
 import java.util.ArrayList;
-/**
- *
- * @author Wanderley de Souza Alencar
- */
-/*
-* Nome.......: TiposDocumentos
-* Objetivo...: Representa os possíveis tipos de documentos que uma pessoa pode
-*              ter para identificar-se
-* Observacoes: Se for desconhecido, utilize o tipo 0 - DS - Desconhecido.
-*/
+
 public class TiposDocumentos {
-    private ArrayList<ElementoDescritivo>           tiposDocumentos;
+    private ArrayList<ElementoDescritivo> tiposDocumentos;
     
     public TiposDocumentos() {
         ElementoDescritivo  elementoDescritivo;
         
-        tiposDocumentos     = new ArrayList();
+        tiposDocumentos     = new ArrayList<ElementoDescritivo>();
         elementoDescritivo  = new ElementoDescritivo();
         elementoDescritivo.setElementoDescritivo(0, "DS", "Desconhecido");
         tiposDocumentos.add(elementoDescritivo); 
@@ -37,17 +28,9 @@ public class TiposDocumentos {
         tiposDocumentos.add(elementoDescritivo); 
     }
     
-    /**
-     * @return A coleção de tipos de documentos.
-     */
     public ArrayList<ElementoDescritivo> getTiposDocumentos() {
         return (this.tiposDocumentos);
     }
-    /**
-     *
-     * @param index Índice, na coleção de tipos de documentos, que se deseja recuperar.
-     * @return O tipo de documento desejado ou, se inexistente, um elemento vazio.
-     */
     public ElementoDescritivo getTipoDocumento(int index) {
         if ((index >= 0) && (index < this.tiposDocumentos.size())) {
             return (this.tiposDocumentos.get(index));
@@ -56,13 +39,7 @@ public class TiposDocumentos {
             return (new ElementoDescritivo());
         }
     }
-    /**
-     * 
-     * @param str A descricao abreviada do tipo de documento a localizado na 
-     *            coleção de tipos de documentos.
-     * @return O índice, na coleção de tipos de documentos, onde está localizado
-     *         o tipo de documento desejado ou -1 se inexistente.
-     */
+   
     public int indexOf(String str) {
         int i;
         for (i = 0; (i <= this.tiposDocumentos.size()); i++) {
--- src/sistemaacademico/TiposLogradouros.java
+++ src/sistemaacademico/TiposLogradouros.java
@@ -2,55 +2,146 @@ package sistemaacademico;
 
 import java.util.ArrayList;
 
-/**
- *
- * @author Wanderley de Souza Alencar
- */
-/*
-* Nome.......: TiposLogradouros
-* Objetivo...: Representa os possíveis tipos de logradouros a que um endereço
-*              pode referir-se
-* Observacoes: Se for desconhecido, utilize o tipo 0 - DS - Desconhecido.
-*/
 public class TiposLogradouros {
-    private ArrayList <ElementoDescritivo>      tiposLogradouros;
-    /**
-     * Implementar de maneira semelhante à UnidadeFederativa.
-     * 
-   Cod. Descrição Comp.   Descrição Abreviada
-   00 - Desconhecido      Des
-   01 - Rua		  Rua
-   02 - Avenida		  Avn
-   03 - Alameda		  Alm
-   04 - Beco		  Bec
-   05 - Boulevard	  Blv
-   06 - Caminho	          Cam
-   07 - Cais		  Cas
-   08 - Campo		  Cmp
-   09 - Escada		  Esc
-   10 - Estrada		  Etr
-   11 - Favela	          Fav
-   12 - Fazenda	 	  Faz
-   13 - Floresta	  Flt
-   14 - Ilha		  Ilh
-   15 - Jardim		  Jrd
-   16 - Ladeira		  Lad
-   17 - Largo		  Lrg
-   18 - Loteamento	  Ltm
-   19 - Lugar             Lug
-   20 - Morro             Mrr
-   21 - Parque		  Pqe
-   22 - Passeio		  Pas
-   23 - Praia             Pra
-   24 - Praça             Prc
-   25 - Recanto           Rec
-   26 - Rodovia           Rod
-   28 - Servidao          Srv
-   29 - Travessa          Trv
-   30 - Via               Via
-   31 - Vila              Vil
-   32 - Outro             Out  
-   * 
-   */
-    
+    private ArrayList <ElementoDescritivo> tiposLogradouros;
+   
+    public TiposLogradouros() {
+        ElementoDescritivo  elementoDescritivo;
+        
+        this.tiposLogradouros = new ArrayList<ElementoDescritivo>();
+        elementoDescritivo  = new ElementoDescritivo();
+        elementoDescritivo.setElementoDescritivo(0, "DES", "Desconhecido");
+        this.tiposLogradouros.add(elementoDescritivo); 
+        elementoDescritivo  = new ElementoDescritivo();
+        elementoDescritivo.setElementoDescritivo(1, "RUA", "Rua");
+        this.tiposLogradouros.add(elementoDescritivo); 
+        elementoDescritivo  = new ElementoDescritivo();
+        elementoDescritivo.setElementoDescritivo(2, "AVN", "Avenida");
+        this.tiposLogradouros.add(elementoDescritivo); 
+        elementoDescritivo  = new ElementoDescritivo();
+        elementoDescritivo.setElementoDescritivo(3, "ALM", "Alameda");
+        this.tiposLogradouros.add(elementoDescritivo); 
+        elementoDescritivo  = new ElementoDescritivo();
+        elementoDescritivo.setElementoDescritivo(4, "BEC", "Beco");
+        this.tiposLogradouros.add(elementoDescritivo); 
+        elementoDescritivo  = new ElementoDescritivo();
+        elementoDescritivo.setElementoDescritivo(5, "BLV", "Boulevard");
+        this.tiposLogradouros.add(elementoDescritivo); 
+        elementoDescritivo  = new ElementoDescritivo();
+        elementoDescritivo.setElementoDescritivo(6, "CAM", "Caminho");
+        this.tiposLogradouros.add(elementoDescritivo); 
+        elementoDescritivo  = new ElementoDescritivo();
+        elementoDescritivo.setElementoDescritivo(7, "CAS", "Cais");
+        this.tiposLogradouros.add(elementoDescritivo); 
+        elementoDescritivo  = new ElementoDescritivo();
+        elementoDescritivo.setElementoDescritivo(8, "CMP", "Campo");
+        this.tiposLogradouros.add(elementoDescritivo); 
+        elementoDescritivo  = new ElementoDescritivo();
+        elementoDescritivo.setElementoDescritivo(9, "ESC", "Escada");
+        this.tiposLogradouros.add(elementoDescritivo);     
+        elementoDescritivo  = new ElementoDescritivo();
+        elementoDescritivo.setElementoDescritivo(10, "ETR", "Estrada");
+        this.tiposLogradouros.add(elementoDescritivo); 
+        elementoDescritivo  = new ElementoDescritivo();
+        elementoDescritivo.setElementoDescritivo(11, "FAV", "Favela");
+        this.tiposLogradouros.add(elementoDescritivo); 
+        elementoDescritivo  = new ElementoDescritivo();
+        elementoDescritivo.setElementoDescritivo(12, "FAZ", "Fazenda");
+        this.tiposLogradouros.add(elementoDescritivo); 
+        elementoDescritivo  = new ElementoDescritivo();
+        elementoDescritivo.setElementoDescritivo(13, "FLT", "Floresta");
+        this.tiposLogradouros.add(elementoDescritivo); 
+        elementoDescritivo  = new ElementoDescritivo();
+        elementoDescritivo.setElementoDescritivo(14, "ILH", "Ilha");
+        this.tiposLogradouros.add(elementoDescritivo); 
+        elementoDescritivo  = new ElementoDescritivo();
+        elementoDescritivo.setElementoDescritivo(15, "JRD", "Jardim");
+        this.tiposLogradouros.add(elementoDescritivo); 
+        elementoDescritivo  = new ElementoDescritivo();
+        elementoDescritivo.setElementoDescritivo(16, "LAD", "Ladeira");
+        this.tiposLogradouros.add(elementoDescritivo); 
+        elementoDescritivo  = new ElementoDescritivo();
+        elementoDescritivo.setElementoDescritivo(17, "LRG", "Largo");
+        this.tiposLogradouros.add(elementoDescritivo); 
+        elementoDescritivo  = new ElementoDescritivo();
+        elementoDescritivo.setElementoDescritivo(18, "LTM", "Loteamento");
+        this.tiposLogradouros.add(elementoDescritivo); 
+        elementoDescritivo  = new ElementoDescritivo();
+        elementoDescritivo.setElementoDescritivo(19, "LUG", "Lugar");
+        this.tiposLogradouros.add(elementoDescritivo); 
+        elementoDescritivo  = new ElementoDescritivo();
+        elementoDescritivo.setElementoDescritivo(20, "MRR", "Morro");
+        this.tiposLogradouros.add(elementoDescritivo); 
+        elementoDescritivo  = new ElementoDescritivo();
+        elementoDescritivo.setElementoDescritivo(21, "PQE", "Parque");
+        this.tiposLogradouros.add(elementoDescritivo); 
+        elementoDescritivo  = new ElementoDescritivo();
+        elementoDescritivo.setElementoDescritivo(22, "PAS", "Passeio");
+        this.tiposLogradouros.add(elementoDescritivo); 
+        elementoDescritivo  = new ElementoDescritivo();
+        elementoDescritivo.setElementoDescritivo(23, "PRA", "Praia");
+        this.tiposLogradouros.add(elementoDescritivo); 
+        elementoDescritivo  = new ElementoDescritivo();
+        elementoDescritivo.setElementoDescritivo(24, "PRC", "Pra�a");
+        this.tiposLogradouros.add(elementoDescritivo); 
+        elementoDescritivo  = new ElementoDescritivo();
+        elementoDescritivo.setElementoDescritivo(25, "REC", "Recanto");
+        this.tiposLogradouros.add(elementoDescritivo); 
+        elementoDescritivo  = new ElementoDescritivo();
+        elementoDescritivo.setElementoDescritivo(26, "ROD", "Rodovia");
+        this.tiposLogradouros.add(elementoDescritivo); 
+        elementoDescritivo  = new ElementoDescritivo();
+        elementoDescritivo.setElementoDescritivo(27, "SRV", "Servid�o");
+        this.tiposLogradouros.add(elementoDescritivo);
+        elementoDescritivo  = new ElementoDescritivo();
+        elementoDescritivo.setElementoDescritivo(28, "TRV", "Travessa");
+        this.tiposLogradouros.add(elementoDescritivo);  
+        elementoDescritivo  = new ElementoDescritivo();
+        elementoDescritivo.setElementoDescritivo(29, "VIA", "Via");
+        this.tiposLogradouros.add(elementoDescritivo);  
+        elementoDescritivo  = new ElementoDescritivo();
+        elementoDescritivo.setElementoDescritivo(30, "VIL", "Vila");
+        this.tiposLogradouros.add(elementoDescritivo);  
+        elementoDescritivo  = new ElementoDescritivo();
+        elementoDescritivo.setElementoDescritivo(31, "OUT", "Outro");
+        this.tiposLogradouros.add(elementoDescritivo);  
+    }
+   
+    public ArrayList<ElementoDescritivo> getTiposLogradouros() {
+        return (this.tiposLogradouros);
+    }
+
+    public ElementoDescritivo getTiposLogradouros(int index) {
+        if ((index >= 0) && (index < tiposLogradouros.size())) {
+            return (this.tiposLogradouros.get(index));
+        }
+        else {
+            return (new ElementoDescritivo());
+        }
+    }
+  
+    public int indexOf(String str) {
+        int i;
+        for (i = 0; (i < this.tiposLogradouros.size()); i++) {
+            if (this.tiposLogradouros.get(i).getDescricaoAbreviada().equalsIgnoreCase(str)) {
+                return (i);
+            }
+        }
+        return (-1);
+    }
+   
+    public int size() {
+        return(this.tiposLogradouros.size());
+    }
+   
+    public void show(int index) {
+        System.out.println();
+        System.out.println("------------------");
+        System.out.println("Tipos de logradouros");
+        System.out.println("------------------");
+        System.out.println("C�digo..........: " + this.getTiposLogradouros(index).getCodigo());
+        System.out.println("Descr. Abreviada: " + this.getTiposLogradouros(index).getDescricaoAbreviada());
+        System.out.println("Descr. Completa.: " + this.getTiposLogradouros(index).getDescricaoCompleta());
+        System.out.println("------------------");
+    }
 }
