1a6972ea53f1e3ced45287dd100a25a2354c64bc
rafaeldziderio <rafaeldezideriovilela@gmail.com>
2018-12-09 14:54:31 -0200

    Acrescentei uma main para realizar a criação de uma pessoa

---
+++ src/sistemaacademico/Objeto.java
@@ -0,0 +1,15 @@
+package sistemaacademico;
+
+public class Objeto {
+	
+	public static void main(String[] args) {
+		System.out.println("Digite o nome: ");
+		//System.out.println();
+		//System.out.println();
+		//System.out.println();
+		//System.out.println();
+		//System.out.println();
+		//System.out.println();
+		
+	}
+}
