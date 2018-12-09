b40ee71fc5a13a82a4c9750651579008a1dcd060
rafaeldziderio <rafaeldezideriovilela@gmail.com>
2018-12-09 18:21:26 -0200

    Ja alterei algumas coisas em Objeto.java

--- src/sistemaacademico/Objeto.java
+++ src/sistemaacademico/Objeto.java
@@ -9,28 +9,51 @@ public class Objeto {
 	public static void main(String[] args) {
 		UnidadesFederativas UF = new UnidadesFederativas();
 		int escolha = 0;
-		String nome;
+		//String nome = "";
 		long matricula=0;
 		boolean a = true;
+		
+		Academico academico = new Academico();
 		
 		
 		while(a) {
 			try {
 				matricula =Long.parseLong(JOptionPane.showInputDialog(null, "Digite sua matrícula: "));
-				
+				break;
 				
 			}catch(NumberFormatException e) {
-				matricula =Long.parseLong(JOptionPane.showInputDialog(null, "Digite sua matrícula: "));
+				JOptionPane.showMessageDialog(null, "Digite uma matrícula válida!");
 				
 			}
 		}
 		JOptionPane.showMessageDialog(null,matricula);
+		academico.setMatricula(matricula);
 		
-		nome = JOptionPane.showInputDialog(null, "Digite seu nome: ");
-		JOptionPane.showMessageDialog(null,nome);
-
 		
-		escolha = Integer.parseInt(JOptionPane.showInputDialog(null,  "Escolha entre as unidades a seguir a partir do CODIGO: \n" + UF.imprimirUF()));
-		JOptionPane.showMessageDialog(null, escolha);
+		//while(a) {
+		//	try {
+		//		nome = JOptionPane.showInputDialog(null, "Digite seu nome: ");
+		//		break;
+		//	}catch(InputMismatchException e) {
+		//		JOptionPane.showMessageDialog(null, "Digite um nome válido!");
+		//	}
+//
+		//}
+		//JOptionPane.showMessageDialog(null,nome);
+			
+		while(a) {	
+			try {
+				escolha = Integer.parseInt(JOptionPane.showInputDialog(null,  "Escolha entre as unidades a seguir a partir do CODIGO: \n" + UF.imprimirUF()));
+				if(escolha<0 || escolha>27) {
+					JOptionPane.showMessageDialog(null, "Digite um número coerente com os códigos:");
+				}
+				else
+					break;
+			}catch(NumberFormatException e) {
+				JOptionPane.showMessageDialog(null, "Digite o código: ");
+			}
+		}
+		JOptionPane.showMessageDialog(null,(UF.getUfs(escolha).getDescricaoCompleta()));
+		//academico.setUfNascimento(escolha);
 	}
 }
