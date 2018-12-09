e54021779f3e480f65d1b8e518b30913adc45b0d
BrunoFerreira18 <brunovaz.1812@gmail.com>
2018-12-09 15:50:21 -0200

    Exemplo usando o JOptionpane

--- src/sistemaacademico/ArrayAcademico.java
+++ src/sistemaacademico/ArrayAcademico.java
@@ -5,7 +5,8 @@ import java.util.ArrayList;
 public class ArrayAcademico {
 	private ArrayList <Academico> academicos = new ArrayList<Academico>();
 	
-	public void cadastrarAcademico(Academico academico) {
+	public void cadastrarAcademico(long matricula, int situacaoAcademica, Pessoa pessoa) {
+		Academico academico = new Academico(matricula, situacaoAcademica, pessoa);
 		academicos.add(academico);
 	}
 	
--- src/sistemaacademico/Objeto.java
+++ src/sistemaacademico/Objeto.java
@@ -1,15 +1,20 @@
 package sistemaacademico;
 
+import javax.swing.JOptionPane;
+
 public class Objeto {
 	
 	public static void main(String[] args) {
-		System.out.println("Digite o nome: ");
-		//System.out.println();
-		//System.out.println();
-		//System.out.println();
-		//System.out.println();
-		//System.out.println();
-		//System.out.println();
+		String nome;
 		
+		nome = JOptionPane.showInputDialog("Digite o nome: "); //Esse lê o que for digitado em formato string
+		JOptionPane.showMessageDialog(null, "Nome digitado: " + nome, "Mostrar o nome digitado", 1); //este só imprimi uma mensagem na tela
+		
+		int codigo;
+		
+		codigo = Integer.parseInt(JOptionPane.showInputDialog("Digite o codigo: ")); //Faz a conversão de String para inteiro
+		JOptionPane.showMessageDialog(null, "Inteiro digitado: " + codigo, "Usar inteiro", 2);  //IMPRIMI NORMAL
+		
+		//dentro do JOptionane.showMENSAGEDIALOG tem  um numero la no final, esse numero é só pra mudar o simbolo que aparece la na tela 
 	}
 }
