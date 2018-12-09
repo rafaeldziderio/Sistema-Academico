6738ea5c89123ceb6e304bd4ab0c34e5594ea191
rafaeldziderio <rafaeldezideriovilela@gmail.com>
2018-12-09 17:43:26 -0200

    Olha ai bruno

--- src/sistemaacademico/Objeto.java
+++ src/sistemaacademico/Objeto.java
@@ -8,9 +8,29 @@ public class Objeto {
 	
 	public static void main(String[] args) {
 		UnidadesFederativas UF = new UnidadesFederativas();
-		int Escolha = 0;
-		Escolha = Integer.parseInt(JOptionPane.showInputDialog(null,  "Escolha entre as unidades a seguir a partir do CODIGO: \n" + UF.imprimirUF()));
+		int escolha = 0;
+		String nome;
+		long matricula=0;
+		boolean a = true;
 		
-		JOptionPane.showMessageDialog(null, Escolha);
+		
+		while(a) {
+			try {
+				matricula =Long.parseLong(JOptionPane.showInputDialog(null, "Digite sua matrícula: "));
+				
+				
+			}catch(NumberFormatException e) {
+				matricula =Long.parseLong(JOptionPane.showInputDialog(null, "Digite sua matrícula: "));
+				
+			}
+		}
+		JOptionPane.showMessageDialog(null,matricula);
+		
+		nome = JOptionPane.showInputDialog(null, "Digite seu nome: ");
+		JOptionPane.showMessageDialog(null,nome);
+
+		
+		escolha = Integer.parseInt(JOptionPane.showInputDialog(null,  "Escolha entre as unidades a seguir a partir do CODIGO: \n" + UF.imprimirUF()));
+		JOptionPane.showMessageDialog(null, escolha);
 	}
 }
