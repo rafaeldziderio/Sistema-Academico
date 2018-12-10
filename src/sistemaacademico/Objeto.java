d7c51a2cd2a5f5f3779cd91a02c81f323b44882e
rafaeldziderio <rafaeldezideriovilela@gmail.com>
2018-12-10 08:04:40 -0200

    Alterei algumas coisas

--- src/sistemaacademico/Objeto.java
+++ src/sistemaacademico/Objeto.java
@@ -7,53 +7,22 @@ import javax.swing.JOptionPane;
 public class Objeto {
 	
 	public static void main(String[] args) {
-		UnidadesFederativas UF = new UnidadesFederativas();
-		int escolha = 0;
-		//String nome = "";
-		long matricula=0;
-		boolean a = true;
-		
-		Academico academico = new Academico();
-		
-		
-		while(a) {
-			try {
-				matricula =Long.parseLong(JOptionPane.showInputDialog(null, "Digite sua matrícula: "));
-				break;
-				
-			}catch(NumberFormatException e) {
-				JOptionPane.showMessageDialog(null, "Digite uma matrícula válida!");
-				
-			}
+		int i= 0;
+		int j =0;
+		do {
+		try {
+			if()
+			i = Integer.parseInt(JOptionPane.showInputDialog(null,"Digite o que quer fazer:\n 1 - Cadastrar\n 2 - Alterar\n 3 - Consultar\n 4 - Excluir"));
+		}catch(NumberFormatException e) {
+			JOptionPane.showMessageDialog(null,"Digite um número de 1 até 4: ");
 		}
-		JOptionPane.showMessageDialog(null,matricula);
-		academico.setMatricula(matricula);
+		JOptionPane.showMessageDialog(null, i);
 		
-		
-		//while(a) {
-		//	try {
-		//		nome = JOptionPane.showInputDialog(null, "Digite seu nome: ");
-		//		break;
-		//	}catch(InputMismatchException e) {
-		//		JOptionPane.showMessageDialog(null, "Digite um nome válido!");
-		//	}
-//
-		//}
-		//JOptionPane.showMessageDialog(null,nome);
+		if(i==1) {
 			
-		while(a) {	
-			try {
-				escolha = Integer.parseInt(JOptionPane.showInputDialog(null,  "Escolha entre as unidades a seguir a partir do CODIGO: \n" + UF.imprimirUF()));
-				if(escolha<0 || escolha>27) {
-					JOptionPane.showMessageDialog(null, "Digite um número coerente com os códigos:");
-				}
-				else
-					break;
-			}catch(NumberFormatException e) {
-				JOptionPane.showMessageDialog(null, "Digite o código: ");
-			}
 		}
-		JOptionPane.showMessageDialog(null,(UF.getUfs(escolha).getDescricaoCompleta()));
-		//academico.setUfNascimento(escolha);
+		JOptionPane.showMessageDialog(null, "Digite 1 para terminar o programa e 0 para continuar");
+		}while(j==0);
+		
 	}
 }
